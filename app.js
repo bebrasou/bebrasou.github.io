a47f2d0b3119ac4537b2f7e614bd32df0a819bef
sexipavel <skibamax447@gmail.com>
2025-09-06 15:58:40 +0300

    2.5
    major update is additions, minor refactoring

--- app.js
+++ app.js
@@ -1,54 +1,117 @@
 
 
 const lessons = {
-    1: {
-        1: "Хімія 9:00",
-        2: "Алгебра 9:55",
-        3: "Всесвітня 10:50",
-        4: "Фіз-ра 12:00",
-        5: "Біологія 12:55",
-        6: "Інформат 13:50",
-    },
-    2: {
-        1: "Геометрія 9:00",
-        2: "Іст. Укр 9:55",
-        3: "Фіз-ра 10:50",
-        4: "Фізика 12:00",
-        5: "-",
-        6: "Мист. 13:50",
-        7: "Осн. здор. 14:45",
-    },
-    3: {
-        1: "-",
-        2: "-",
-        3: "Зар. літ 10:50",
-        4: "-",
-        5: "-",
-        6: "Англ 13:50",
-        7: "Укр. мова 14:45",
-        8: "Укр. літ 15:40",
-    },
-    4: {
-        1: "-",
-        2: "Англ 9:55",
-        3: "-",
-        4: "-",
-        5: "-",
-        6: "Фізика 13:50",
-        7: "Труди 14:45",
-        8: "-",
-        9: "-",
-    },
-    5: {
-        1: "-",
-        2: "Правознав 9:55",
-        3: "-",
-        4: "Географія 12:00",
-        5: "-",
-        6: "-",
-        7: "-",
-    },
-};
+            1: { // Понеділок
+                1: "Укр. мова 9:00",
+                2: "Укр. літ 9:55",
+                3: "Географія 10:50",
+                4: "Історія України 12:00",
+                5: "Фіз-ра 12:55",
+                6: "-",
+                7: "Громад. освіта 14:45",
+            },
+            2: { // Вівторок
+                1: "Фіз-ра 9:00",
+                2: "Укр. мова 9:55",
+                3: "Укр. літ 10:50",
+                4: "Алгебра 12:00",
+                5: "Фізика 12:55",
+                6: "Захист України 13:50",
+                7: "Мистецтво / Інформ 14:45",
+            },
+            3: { // Середа
+                1: "Укр. мова 9:00",
+                2: "Англійська 9:55",
+                3: "Укр. літ 10:50",
+                4: "Геометрія 12:00",
+                5: "Громад. освіта 12:55",
+                6: "Мистецтво 13:50",
+                7: "-",
+                8: "-",
+            },
+            4: { // Четвер
+                1: "Всесвіт. історія 9:00",
+                2: "Інформатика 9:55",
+                3: "-",
+                4: "Захист України 12:00",
+                5: "Заруб. літ 12:55",
+                6: "Англійська 13:50",
+                7: "Фізика 14:45",
+            },
+            5: { // П'ятниця
+                1: "-",
+                2: "Біологія 9:55",
+                3: "-",
+                4: "Хімія 12:00",
+                5: "-",
+                6: "-",
+                7: "-",
+            },
+        };
+
+function timeLesson(skipLesson) {
+    const startDate = new Date("2025-01-04");
+    const currentDate = new Date();
+
+    const weeksPassed = Math.floor(
+        (currentDate - startDate) / (7 * 24 * 60 * 60 * 1000)
+    );
+
+    const value = weeksPassed % 2 === 0 ? 1 : 2;
+
+    const currentDay = currentDate.getDay();
+    const currentHour = currentDate.getHours();
+    const currentMinute = currentDate.getMinutes();
+    let timeLesson = 10;
+
+    // if (
+    //     currentDay === 5 &&
+    //     currentHour === 9 &&
+    //     currentMinute < 55 &&
+    //     value === 1
+    // )
+    //     timeLesson = 4; // Всесвіт
+
+    if (currentHour < 9) timeLesson = 1;
+    if (currentHour === 9 && currentMinute < 55) timeLesson = 2;
+
+    if (
+        (currentHour === 9 && currentMinute > 55) ||
+        (currentHour === 10 && currentMinute < 50)
+    )
+        timeLesson = 3;
+    if (
+        (currentHour === 10 && currentMinute > 50) ||
+        (currentHour === 11 && currentMinute < 59)
+    )
+        timeLesson = 4;
+    if (
+        (currentHour === 12 && currentMinute > 0) ||
+        (currentHour === 12 && currentMinute < 55)
+    )
+        timeLesson = 5;
+    if (
+        (currentHour === 12 && currentMinute > 55) ||
+        (currentHour === 13 && currentMinute < 50)
+    )
+        timeLesson = 6;
+    if (
+        (currentHour === 13 && currentMinute > 50) ||
+        (currentHour === 14 && currentMinute < 45)
+    )
+        timeLesson = 7;
+    if (
+        (currentHour === 14 && currentMinute > 45) ||
+        (currentHour === 15 && currentMinute < 40)
+    )
+        timeLesson = 8;
+
+    if (!skipLesson) {
+        return timeLesson;
+    } else {
+        return timeLesson + skipLesson;
+    }
+}
 
 
 function clockNow() {
@@ -65,22 +128,20 @@ function clockNow() {
     let hour = currentDate.getHours();
     let min = currentDate.getMinutes();
     let sec = currentDate.getSeconds();
-    let pi = currentDate.getDay() - 1;
-    if (pi === -1) pi = 6;
-    const day = days[pi];
+    let curDay = currentDate.getDay() - 1;
+    if (curDay === -1) curDay = 6;
+    const day = days[curDay];
     if (hour < 10) hour = "0" + hour;
     if (min < 10) min = "0" + min;
     if (sec < 10) sec = "0" + sec;
-    let test = day + " " + hour + ":" + min + ":" + sec;
+    let timeNow = day + " " + hour + ":" + min + ":" + sec;
 
     const clock = document.getElementById("clocknow");
-    clock.textContent = test;
-
-    console.log(clock.textContent);
+    clock.textContent = timeNow;
 }
 setInterval(clockNow, 1000);
 
-function blur() {
+function order() {
     let currentDate = new Date();
     const day = currentDate.getDay();
     const days = {
@@ -112,112 +173,27 @@ function blur() {
 }
 
 function nextLesson() {
-    const startDate = new Date("2025-01-04");
-    const currentDate = new Date();
 
-    const weeksPassed = Math.floor(
-        (currentDate - startDate) / (7 * 24 * 60 * 60 * 1000)
-    );
-
-    const value = weeksPassed % 2 === 0 ? 1 : 2;
-
+    let currentDate = new Date();
     const currentDay = currentDate.getDay();
-    const currentHour = currentDate.getHours();
-    const currentMinute = currentDate.getMinutes();
-    let uroktime = 10;
 
-    if (currentHour < 9) uroktime = 1;
-    if (currentHour === 9 && currentMinute < 55) uroktime = 2;
-    if (
-        currentDay === 5 &&
-        currentHour === 9 &&
-        currentMinute < 55 &&
-        value === 1
-    )
-        uroktime = 4; // Правознавство
-    if (
-        (currentHour === 9 && currentMinute > 55) ||
-        (currentHour === 10 && currentMinute < 50)
-    )
-        uroktime = 3;
-    if (
-        (currentDay === 1 &&
-            currentHour === 9 &&
-            currentMinute > 55 &&
-            value === 2) ||
-        (currentDay === 1 &&
-            currentHour === 10 &&
-            currentMinute < 50 &&
-            value === 2)
-    )
-        uroktime = 4; // Всесвітня історія
-    if (
-        (currentHour === 10 && currentMinute > 50) ||
-        (currentHour === 11 && currentMinute < 59)
-    )
-        uroktime = 4;
-    if (
-        (currentHour === 12 && currentMinute > 0) ||
-        (currentHour === 12 && currentMinute < 55)
-    )
-        uroktime = 5;
-    if (
-        (currentHour === 12 && currentMinute > 55) ||
-        (currentHour === 13 && currentMinute < 50)
-    )
-        uroktime = 6;
-    if (
-        (currentDay === 2 &&
-            currentHour === 12 &&
-            currentMinute > 55 &&
-            value === 2) ||
-        (currentDay === 2 &&
-            currentHour === 13 &&
-            currentMinute < 50 &&
-            value === 2)
-    )
-        uroktime = 7;
-    if (
-        (currentHour === 13 && currentMinute > 50) ||
-        (currentHour === 14 && currentMinute < 45)
-    )
-        uroktime = 7;
-    if (
-        (currentDay === 2 &&
-            currentHour === 13 &&
-            currentMinute > 50 &&
-            value === 1) ||
-        (currentDay === 2 &&
-            currentHour === 14 &&
-            currentMinute < 45 &&
-            value === 1)
-    )
-        uroktime = 10;
-    if (
-        (currentHour === 14 && currentMinute > 45) ||
-        (currentHour === 15 && currentMinute < 40)
-    )
-        uroktime = 8;
-    if (
-        (currentHour === 15 && currentMinute > 40) ||
-        (currentHour === 16 && currentMinute < 35)
-    )
-        uroktime = 9;
+    let skipLesson = 0;
 
-
+    let time = timeLesson();
 
     if (!lessons[currentDay]) {
-        document.getElementById("lessonName").textContent = "Выходной!";
+        document.getElementById("lessonName").textContent = "Вихідний!";
         return;
     }
 
-    let currentLesson = lessons[currentDay][uroktime];
+    let currentLesson = lessons[currentDay][time];
 
     const lesson = document.getElementById("lessonName");
-    if (uroktime !== 10) {
+    if (time !== 10) {
         while (currentLesson === "-") {
-            uroktime++;
-            currentLesson = lessons[currentDay][uroktime];
+            skipLesson++;
+            time = timeLesson(skipLesson);
+            currentLesson = lessons[currentDay][time];
         }
         if (currentLesson === undefined) {
             lesson.textContent = "Уроки завтра!"
@@ -228,98 +204,8 @@ function nextLesson() {
 setInterval(nextLesson, 1000);
 
 function countDown() {
-    const startDate = new Date("2025-01-04");
     const currentDate = new Date();
-
-    const weeksPassed = Math.floor(
-        (currentDate - startDate) / (7 * 24 * 60 * 60 * 1000)
-    );
-
-    const value = weeksPassed % 2 === 0 ? 1 : 2;
-    const clock = document.getElementById("countDown");
     const currentDay = currentDate.getDay();
-    const currentHour = currentDate.getHours();
-    const currentMinute = currentDate.getMinutes();
-
-    let lessonTime = 10;
-
-    if (currentHour < 9) lessonTime = 1;
-    if (currentHour === 9 && currentMinute < 55) lessonTime = 2;
-    if (
-        currentDay === 5 &&
-        currentHour === 9 &&
-        currentMinute < 55 &&
-        value === 1
-    )
-        lessonTime = 4;
-    if (
-        (currentHour === 9 && currentMinute > 55) ||
-        (currentHour === 10 && currentMinute < 50)
-    )
-        lessonTime = 3;
-    if (
-        (currentDay === 1 &&
-            currentHour === 9 &&
-            currentMinute > 55 &&
-            value === 2) ||
-        (currentDay === 1 &&
-            currentHour === 10 &&
-            currentMinute < 50 &&
-            value === 2)
-    )
-        lessonTime = 4;
-    if (
-        (currentHour === 10 && currentMinute > 50) ||
-        (currentHour === 11 && currentMinute < 59)
-    )
-        lessonTime = 4;
-    if (
-        (currentHour === 12 && currentMinute > 0) ||
-        (currentHour === 12 && currentMinute < 55)
-    )
-        lessonTime = 5;
-    if (
-        (currentHour === 12 && currentMinute > 55) ||
-        (currentHour === 13 && currentMinute < 50)
-    )
-        lessonTime = 6;
-    if (
-        (currentDay === 2 &&
-            currentHour === 12 &&
-            currentMinute > 55 &&
-            value === 2) ||
-        (currentDay === 2 &&
-            currentHour === 13 &&
-            currentMinute < 50 &&
-            value === 2)
-    )
-        lessonTime = 7;
-    if (
-        (currentHour === 13 && currentMinute > 50) ||
-        (currentHour === 14 && currentMinute < 45)
-    )
-        lessonTime = 7;
-    if (
-        (currentDay === 2 &&
-            currentHour === 13 &&
-            currentMinute > 50 &&
-            value === 1) ||
-        (currentDay === 2 &&
-            currentHour === 14 &&
-            currentMinute < 45 &&
-            value === 1)
-    )
-        lessonTime = 10;
-    if (
-        (currentHour === 14 && currentMinute > 45) ||
-        (currentHour === 15 && currentMinute < 40)
-    )
-        lessonTime = 8;
-    if (
-        (currentHour === 15 && currentMinute > 40) ||
-        (currentHour === 16 && currentMinute < 35)
-    )
-        lessonTime = 9;
 
     const time = {
         1: "9:00:00",
@@ -330,44 +216,37 @@ function countDown() {
         6: "13:50:00",
         7: "14:45:00",
         8: "15:40:00",
-        9: "16:35:00",
     };
 
     if (!lessons[currentDay]) {
-        document.getElementById("countDown").textContent = "Выходной!";
+        document.getElementById("countDown").textContent = "Вихідний!";
         return;
     }
 
+    // получаем следующий урок через timeLesson
+    let lessonTime = timeLesson();
     let currentLesson = lessons[currentDay][lessonTime];
 
-    if (lessonTime !== 10) {
-        while (currentLesson === "-") {
-            lessonTime++;
-            currentLesson = lessons[currentDay][lessonTime];
-        }
+    while (currentLesson === "-") {
+        lessonTime = timeLesson(++lessonTime);
+        currentLesson = lessons[currentDay][lessonTime];
     }
 
-    const timeLeft = time[lessonTime];
-
-    let deadline = `${currentDate.toDateString()} ${timeLeft} GMT+02:00`;
-
-    let t = Date.parse(deadline) - Date.parse(new Date());
-    let seconds = Math.floor((t / 1000) % 60);
-    let minutes = Math.floor((t / 1000 / 60) % 60);
-    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    if (hours < 10) hours = "0" + hours;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (seconds < 10) seconds = "0" + seconds;
-
-    let leftTime = `${hours}:${minutes}:${seconds}`;
-
-    if (lessonTime !== 10 && currentLesson !== undefined) {
-        clock.textContent = leftTime;
-        console.log(leftTime);
-    } else {
-        clock.textContent = "Уроки завтра!";
+    if (!currentLesson) {
+        document.getElementById("countDown").textContent = "Уроки завтра!";
+        return;
     }
+
+    const deadline = new Date(`${currentDate.toDateString()} ${time[lessonTime]} GMT+02:00`);
+    const t = deadline - currentDate;
+
+    let seconds = String(Math.floor((t / 1000) % 60)).padStart(2, "0");
+    let minutes = String(Math.floor((t / 1000 / 60) % 60)).padStart(2, "0");
+    let hours = String(Math.floor((t / (1000 * 60 * 60)) % 24)).padStart(2, "0");
+
+    document.getElementById("countDown").textContent = `${hours}:${minutes}:${seconds}`;
 }
+
 setInterval(countDown, 1000);
 
 document.getElementById("clock").addEventListener("click", () => {
@@ -384,34 +263,30 @@ document.getElementById("clock").addEventListener("click", () => {
     }
 });
 
+
 function change() {
     const startDate = new Date("2025-01-04");
-    let currentDate = new Date();
+    const currentDate = new Date();
 
-    let weeksPassed = Math.floor((currentDate - startDate) / (7 * 24 * 60 * 60 * 1000));
+    const weeksPassed = Math.floor(
+        (currentDate - startDate) / (7 * 24 * 60 * 60 * 1000)
+    );
 
-    let value = (weeksPassed % 2 === 0) ? 1 : 2;
+    const value = weeksPassed % 2 === 0 ? 1 : 2;
 
-    let smenaClass = (value === 1) ? "smena1" : "smena2";
-    let text = (value === 1) ? "Історія" : "Географія";
+    const firstWeek = document.querySelectorAll(".firstWeek");
 
-    const rows = document.querySelectorAll(".container ." + smenaClass);
-
-    for (let i = 0; i < rows.length; i++) {
-        rows[i].classList.add("smena");
-    }
-
-    const sre = document.getElementById("sre");
-    if (sre) {
-        sre.textContent = text;
+    if (value === 2) {
+        for (let i = 0; i < firstWeek.length; i++) {
+            firstWeek[i].classList.add("asyncRow");
+        }
     }
 }
 
 
-
 window.onload = function () {
-    blur()
+    order()
     nextLesson()
     clockNow()
-    change();
+    change()
 };
